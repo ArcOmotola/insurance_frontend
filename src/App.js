aa1d8603010ba567bd10916eeb2e176ebe565854
Omotola Ogunsola <ogunsola40@gmail.com>
2022-04-25 11:44:40 +0100

    setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import EnterPhone from "./Pages/EnterPhone";
 
 function App() {
   return (
-    <div className="App">
-      Aella Care
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      <EnterPhone />
+    </h1>
   );
 }
 
 export default App;
+
+
+
---
+++ src/Pages/EnterPhone.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EnterPhone() {
+    return (
+        <div>EnterPhone</div>
+    )
+}
